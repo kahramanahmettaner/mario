992df7c1daf890c5d629b7a2e0d4ccbb9bb58ea1
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2023-08-24 15:48:45 +0200

    added Texture class

---
+++ src/main/java/renderer/Texture.java
@@ -0,0 +1,63 @@
+package renderer;
+
+import org.lwjgl.BufferUtils;
+
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.stb.STBImage.*;
+
+public class Texture {
+    private String filepath;
+    private int texID;
+
+    public Texture(String filepath) {
+        this.filepath = filepath;
+
+        // Generate texture on GPU
+        texID = glGenTextures();
+        glBindTexture(GL_TEXTURE_2D, texID);
+
+        // Set texture parameters
+        // Repeat image in both directions
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); // u
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT); // v
+
+        // When stretching the image, pixelate
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        // When shrinking the image, pixelate
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+        IntBuffer width = BufferUtils.createIntBuffer(1);
+        IntBuffer height = BufferUtils.createIntBuffer(1);
+        IntBuffer channels = BufferUtils.createIntBuffer(1);
+        stbi_set_flip_vertically_on_load(true); // to flip loaded texture's on the y-axis
+        ByteBuffer image = stbi_load(filepath, width, height, channels, 0);
+
+        if (image != null) {
+            if(channels.get(0) == 3) {
+                // internalformat: for mipmap levels, hier: 0 -> no mipmap levels
+                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width.get(0), height.get(0), 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+            }  else if (channels.get(0) == 4) {
+                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width.get(0), height.get(0), 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+            } else {
+                assert false : "Error: (Texture) Unknown number of channels '" + channels.get(0) + "'";
+            }
+
+        } else {
+            assert false: "Error: (Texture) could not load image '" + filepath + "'";
+        }
+
+        stbi_image_free(image);
+
+    }
+
+    public void bind() {
+        glBindTexture(GL_TEXTURE_2D, texID);
+    }
+
+    public void unbind() {
+        glBindTexture(GL_TEXTURE_2D, 0);
+    }
+}
