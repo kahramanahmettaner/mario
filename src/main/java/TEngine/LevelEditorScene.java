1392a87cd72d4e4e55c9fb77a01ca7ca7ad188fa
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2023-08-25 14:55:19 +0200

    Entity Component System
    added the classes Component, GameObject, SpriteRenderer and FontRenderer

--- src/main/java/TEngine/LevelEditorScene.java
+++ src/main/java/TEngine/LevelEditorScene.java
@@ -1,5 +1,7 @@
 package TEngine;
 
+import components.FontRenderer;
+import components.SpriteRenderer;
 import org.joml.Vector2f;
 import org.lwjgl.BufferUtils;
 import renderer.Shader;
@@ -56,6 +58,8 @@ public class LevelEditorScene extends Scene{
     private int vaoID, vboID, eboID;
     private Shader defaultShader;
     private Texture testTexture;
+    GameObject testObj;
+    private boolean firstTime = false;
 
     public LevelEditorScene() {
 
@@ -63,6 +67,12 @@ public class LevelEditorScene extends Scene{
 
     @Override
     public void init() {
+        System.out.println("Creating test object");
+        this.testObj = new GameObject("test object");
+        this.testObj.addComponent(new SpriteRenderer());
+        this.testObj.addComponent(new FontRenderer());
+        this.addGameObjectToScene(this.testObj);
+
         this.camera = new Camera(new Vector2f()); // (0, 0)
         defaultShader = new Shader("assets/shaders/default.glsl");
         defaultShader.compile();
@@ -136,6 +146,19 @@ public class LevelEditorScene extends Scene{
         glBindVertexArray(0);
 
         defaultShader.detach();
+
+        if(!firstTime) {
+            System.out.println("Creating gameObject!");
+            GameObject go = new GameObject("Game Test 2");
+            go.addComponent(new SpriteRenderer());
+            this.addGameObjectToScene(go);
+            firstTime = true;
+        }
+
+        for (GameObject go : this.gameObjects) {
+            go.update(dt);
+        }
+
     }
 
 
