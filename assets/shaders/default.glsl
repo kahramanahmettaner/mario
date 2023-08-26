0536a75d83836f8f3d385cc43447987567cfe44e
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2023-08-26 20:26:05 +0200

    Batch Rendering

--- assets/shaders/default.glsl
+++ assets/shaders/default.glsl
@@ -3,18 +3,15 @@
 
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec4 aColor;
-layout (location = 2) in vec2 aTexCoords;
 
 uniform mat4 uProjection;
 uniform mat4 uView;
 
 out vec4 fColor;
-out vec2 fTexCoords;
 
 void main()
 {
     fColor = aColor;
-    fTexCoords = aTexCoords;
     gl_Position = uProjection * uView * vec4(aPos, 1.0);
 }
 
@@ -22,14 +19,10 @@ void main()
 #version 330 core
 
 in vec4 fColor;
-in vec2 fTexCoords;
-
-uniform float uTime;
-uniform sampler2D TEX_SAMPLER;
 
 out vec4 color;
 
 void main()
 {
-    color = texture(TEX_SAMPLER, fTexCoords);
+    color = fColor;
 }
