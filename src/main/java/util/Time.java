0b91b00b4d7e879ecfa101d2b4ead9f6214accaa
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2023-08-28 22:46:07 +0200

    ´fixed the issue with time
    deleted the class Time and now glfwGetTime is being used instead

--- src/main/java/util/Time.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-public class Time {
-    public static float timeStarted = System.nanoTime(); // a static variable is initialized at the application start!
-
-    public static float getTime() {
-        return (float) ((System.nanoTime() - timeStarted) * 1E-9); // 1E-9 to convert ns to sec
-    }
-}
