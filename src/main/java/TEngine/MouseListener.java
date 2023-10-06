1a0847c060ddc3e7f57692c80d1dbcc58a841d88
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2023-10-06 19:33:41 +0200

    Add Hover Color Fucntionaility to Gizmo Arrows
    Now, when a user hovers the mouse cursor over the gizmo arrows, they change color to provide visual feedback and indicate interactivity.

--- src/main/java/TEngine/MouseListener.java
+++ src/main/java/TEngine/MouseListener.java
@@ -10,9 +10,11 @@ import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
 public class MouseListener {
     private static MouseListener instance;
     private double scrollX, scrollY;
-    private double xPos, yPos, lastX, lastY;
+    private double xPos, yPos, lastX, lastY, worldX, worldY, lastWorldX, lastWorldY;
     private boolean mouseButtonPressed[] = new boolean[9];
     private boolean isDragging;
+
+    private int mouseButtonDown = 0;
 
     private Vector2f gameViewportPos = new Vector2f();
     private Vector2f gameViewportSize = new Vector2f();
@@ -36,20 +38,31 @@ public class MouseListener {
     // Create callbacks
     // GLFW input guide (note: in C and a little bit differently): https://www.glfw.org/docs/3.3/input_guide.html
     public static void mousePosCallback(long window, double xPos, double yPos) {
+        if (get().mouseButtonDown > 0) {
+            get().isDragging = true;
+        }
+
         get().lastX = get().xPos;
         get().lastY = get().yPos;
+        get().lastWorldX = get().worldX;
+        get().lastWorldY = get().worldY;
         get().xPos = xPos;
         get().yPos = yPos;
-        get().isDragging = get().mouseButtonPressed[0] || get().mouseButtonPressed[1] || get().mouseButtonPressed[2];
+        calcOrthoX();
+        calcOrthoY();
     }
 
     public static void mouseButtonCallback(long window, int button, int action, int mods) {
         if (action == GLFW_PRESS) {
+            get().mouseButtonDown++;
+
             if (button < get().mouseButtonPressed.length) {
                 get().mouseButtonPressed[button] = true;
             }
         }
-        if (action == GLFW_RELEASE) {
+        else if (action == GLFW_RELEASE) {
+            get().mouseButtonDown--;
+
             if (button < get().mouseButtonPressed.length) {
                 get().mouseButtonPressed[button] = false;
                 get().isDragging = false;
@@ -81,8 +94,16 @@ public class MouseListener {
         return (float) ( get().lastX - get().xPos );
     }
 
+    public static float getWorldDx() {
+        return (float) ( get().lastWorldX - get().worldX );
+    }
+
     public static float getDy() {
         return (float) ( get().lastY - get().yPos );
+    }
+
+    public static float getWorldDy() {
+        return (float) ( get().lastWorldY - get().worldY );
     }
 
     public static float getScrollX() {
@@ -120,6 +141,10 @@ public class MouseListener {
     }
 
     public static float getOrthoX() {
+        return (float)get().worldX;
+    }
+
+    public static void calcOrthoX() {
         float currentX = getX()- get().gameViewportPos.x;
         currentX = (currentX / get().gameViewportSize.x) * 2.0f - 1.0f;
         Vector4f tmp = new Vector4f(currentX, 0, 0, 1);
@@ -128,12 +153,15 @@ public class MouseListener {
         Matrix4f viewProjection = new Matrix4f();
         camera.getInverseView().mul(camera.getInverseProjection(), viewProjection);
         tmp.mul(viewProjection);
-        currentX = tmp.x;
 
-        return currentX;
+        get().worldX = tmp.x;
     }
 
     public static float getOrthoY() {
+        return (float)get().worldY;
+    }
+
+    public static void calcOrthoY() {
         float currentY = getY() - get().gameViewportPos.y;
         currentY = -((currentY / get().gameViewportSize.y) * 2.0f  - 1.0f);
         Vector4f tmp = new Vector4f(0, currentY, 0, 1);
@@ -142,10 +170,10 @@ public class MouseListener {
         Matrix4f viewProjection = new Matrix4f();
         camera.getInverseView().mul(camera.getInverseProjection(), viewProjection);
         tmp.mul(viewProjection);
-        currentY = tmp.y;
 
-        return currentY;
+        get().worldY = tmp.y;
     }
+
 
     public static void setGameViewportPos(Vector2f gameViewportPos) {
         get().gameViewportPos.set(gameViewportPos);
