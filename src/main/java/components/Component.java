9896fe866358d90cdab59a6152b749a89bc28edd
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2023-11-01 17:31:18 +0100

    Add enum support for imgui fields
    this change helps with futures with the num type e.g. to specify BodyType of an object

--- src/main/java/components/Component.java
+++ src/main/java/components/Component.java
@@ -3,6 +3,7 @@ package components;
 import TEngine.GameObject;
 import editor.TImGui;
 import imgui.ImGui;
+import imgui.type.ImInt;
 import org.joml.Vector2f;
 import org.joml.Vector3f;
 import org.joml.Vector4f;
@@ -72,6 +73,13 @@ public abstract class Component {
                     if (ImGui.dragFloat4(name + ": ", imVec)) {
                         val.set(imVec[0], imVec[1], imVec[2], imVec[3]);
                     }
+                } else if (type.isEnum()) {
+                    String[] enumValues = getEnumValues(type);
+                    String enumType = ((Enum)value).name();
+                    ImInt index = new ImInt(indexOf(enumType, enumValues));
+                    if (ImGui.combo(field.getName(), index, enumValues, enumValues.length)) {
+                        field.set(this, type.getEnumConstants()[index.get()]);
+                    }
                 }
 
                 if (isPrivate) {
@@ -87,6 +95,23 @@ public abstract class Component {
         if (this.uid == -1) {
             this.uid = ID_COUNTER++;
         }
+    }
+
+    private <T extends Enum<T>> String[] getEnumValues(Class<T> enumType) {
+        String[] enumValues = new String[enumType.getEnumConstants().length];
+        int i = 0;
+        for (T enumIntegerValue : enumType.getEnumConstants()) {
+            enumValues[i] = enumIntegerValue.name();
+            i++;
+        }
+        return enumValues;
+    }
+
+    private int indexOf(String str, String[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            return i;
+        }
+        return -1;
     }
 
     public void destroy() {
